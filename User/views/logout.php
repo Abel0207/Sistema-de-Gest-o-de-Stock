e0d7d52cf70d03a245d10dbb8dd143f86781ca77
Abel0207 <abelcanas92@gmail.com>
2023-04-07 19:26:33 +0100

    Nova actualização feita no dia 07/04/2023

--- User/views/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require '../../config.php';
-    session_start();
-    session_destroy();
-    echo "<script>alert('Secção terminada')</script>";
-    echo "<script>window.location = '../../login.php'</script>";
-?>
